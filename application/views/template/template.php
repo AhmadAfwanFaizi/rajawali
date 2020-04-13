a64c91d514fafefaee23c14acf2beef5888444b4
ahmadafwanfaizi <ahmadafwanfaizi@gmail.com>
2020-04-13 15:23:58 +0700

    fixing alert

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -426,7 +426,7 @@
 <div class="modal fade alertModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm" role="document" style="margin: 4% 33%">
     <div class="modal-content" style="border-radius: 3px; width: 500px; text-align: center;">
-      <div id="warnaAlert" class="alert alert-dismissible "> <!--  alert-success -->
+      <div id="warnaAlert" class=""> <!--  alert-success -->
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
         <h4><i id="iconAlert" class=""></i> <span id="judulAlert"></span></h4> <!-- icon fa fa-check -->
         <div id="isiAlert"></div>
@@ -498,29 +498,34 @@
 
   function modalAlert(res, content)
   {
+    $('#warnaAlert').attr('class', '');
+    $('#iconAlert').attr('class', '');
+    $('#judulAlert').text('');
+    $('#isiAlert').html('');
+
     var warnaAlert, iconAlert, judulAlert, isiAlert;
     if(res == "success") {
-      warnaAlert = " alert-success";
+      warnaAlert = "alert alert-dismissible alert-success";
       iconAlert  = " icon fa fa-check";
       judulAlert = "BERHASIL";
       isiAlert   = content;
     } else if(res == "danger") {
-      warnaAlert = " alert-danger";
+      warnaAlert = "alert alert-dismissible alert-danger";
       iconAlert  = " icon fa fa-ban";
       judulAlert = "PERHATIAN";
       isiAlert   = content;
     } else if(res == "warning") {
-      warnaAlert = " alert-warning";
+      warnaAlert = "alert alert-dismissible alert-warning";
       iconAlert  = " icon fa fa-warning";
       judulAlert = "PERHATIAN";
       isiAlert   = content;
     } else if(res == "info") {
-      warnaAlert = " alert-info";
+      warnaAlert = "alert alert-dismissible alert-info";
       iconAlert  = " icon fa fa-info";
       judulAlert = "PEMBERITAHUAN";
       isiAlert   = content;
     } else {
-      warnaAlert = " alert-danger";
+      warnaAlert = "alert alert-dismissible alert-danger";
       iconAlert  = " icon fa fa-ban";
       judulAlert = "PERHATIAN";
       isiAlert   = " PERHATIKAN PARAMETER SCRIPT";
