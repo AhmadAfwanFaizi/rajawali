45dbce17bd23501410044212f183bfafa69ed9d6
ahmad afwan faizi <ahmadafwanfaizi@gmail.com>
2021-02-07 02:30:43 +0700

    perapihan data

--- application/helpers/function_helper.php
+++ application/helpers/function_helper.php
@@ -14,12 +14,6 @@ function waktu_sekarang()
     return date('Y-m-d H:i:s');
 }
 
-function barcode($param)
-{
-    $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-    return $generator->getBarcode($param, $generator::TYPE_CODE_128);
-}
-
 function menu($param, $param2)
 {
     $ci = &get_instance();
@@ -28,25 +22,6 @@ function menu($param, $param2)
     } else {
         return null;
     }
-}
-
-function pdfGenerator($html, $fileName, $paper, $orientation)
-{
-    // reference the Dompdf namespace
-    // use Dompdf\Dompdf;
-
-    // instantiate and use the dompdf class
-    $dompdf = new Dompdf\Dompdf();
-    $dompdf->loadHtml($html);
-
-    // (Optional) Setup the paper size and orientation
-    $dompdf->setPaper($paper, $orientation);
-
-    // Render the HTML as PDF
-    $dompdf->render();
-
-    // Output the generated PDF to Browser
-    $dompdf->stream($fileName, ['Attachment' => 0]);
 }
 
 function notif($alert = null, $text = null)
